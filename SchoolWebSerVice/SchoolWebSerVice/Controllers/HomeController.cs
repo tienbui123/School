64ff5c87354da037cf7dc3121f92259190ecae33
tienbui123 <minhtiendvl@gmail.com>
2015-08-05 19:26:45 +0700

    Ung dung TTDT

---
+++ SchoolWebSerVice/SchoolWebSerVice/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using SchoolWebSerVice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolWebSerVice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Messenger = GetHTML.getTKB(GetHTML.makeUrlTKB("3111410094","1"));
+            return View();
+        }
+    }
+}
---
+++ SchoolWebSerVice/SchoolWebSerVice/Models/GetHTML.cs
@@ -0,0 +1,55 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace SchoolWebSerVice.Models
+{
+    public class GetHTML
+    {
+        static string url = "http://thongtindaotao.sgu.edu.vn/default.aspx?";
+
+
+        public static string getTKB(string url)
+        {
+
+             HtmlWeb htmlWeb = new HtmlWeb();
+             string str="";
+             HtmlDocument document = htmlWeb.Load(url);
+             HtmlNodeCollection nodes = document.DocumentNode.SelectNodes("//*[@id='ctl00_ContentPlaceHolder1_ctl00_lblContentTenSV']");
+             if (nodes != null)
+             {
+                 foreach (var node in nodes)
+                 {
+                     str = node.InnerText;
+                 }
+             }
+             return str;
+        }
+
+        public static string makeUrlTKB(string msv, string hocky)
+        {
+            string data = url + "page=thoikhoabieu&sta=1&id="+msv;
+            
+            return data;
+        }
+        public static string makeUrlDT(string msv, string hocky)
+        {
+            string data = url + "page=xemdiemthi&sta=1&id=" + msv;
+
+            return data;
+        }
+        public static string makeUrlLT(string msv, string hocky)
+        {
+            string data = url + "page=xemlichthi&sta=1&id=" + msv;
+
+            return data;
+        }
+
+
+    }
+}
